4186d757bf32412b485cf1a6c3c0676a564e088b
x9SunZ <116784765+x9SunZ@users.noreply.github.com>
2024-03-09 10:14:11 +0700

    Update Loader.lua

--- Loader/main/Loader.lua
+++ Loader/main/Loader.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 142823291 then
-
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/x9SunZ/Serity-Trash/main/Serity%20Hub%20-%20Muder%20Mystery%202'))()
 else
     game.Players.LocalPlayer:Kick("Serity Hub : Not Found Place ID")
 end
